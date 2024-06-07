5512ff7db869fdb2b012b9c7bbc42320b7de2857
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2024-06-07 11:20:52 +0200

    Revert "Create read_fasta function with tests"
    This reverts commit dd413bfe743fb8e1c3de23c44363a52e29f59601.

--- benchmarking/vignette.R
+++ benchmarking/vignette.R
@@ -1,24 +1,35 @@
 library(tidyverse)
 
-fasta <- "benchmarking/trainset19_072023.rdp/trainset19_072023.rdp.fasta"
 taxonomy <- "benchmarking/trainset19_072023.rdp/trainset19_072023.rdp.tax"
-
-
-fasta_df <- read_fasta(fasta)
+fasta <- "benchmarking/trainset19_072023.rdp/trainset19_072023.rdp.fasta"
 
 genera <- read_tsv(taxonomy,
                    col_names = c("accession", "taxonomy")) |>
   mutate(taxonomy = stringi::stri_replace_all_regex(taxonomy, ";$", ""))
 
+fasta_data <- scan(fasta,
+                   sep = "\n",
+                   what = character(), quiet = TRUE)
 
+sequence_names <- fasta_data[seq(1, length(fasta_data), by = 2)] |>
+  str_replace_all(pattern = "^>([^\t]*)\t.*", "\\1")
 
-seq_table <- as_tibble(fasta_df) |>
-  inner_join(genera, by = c("id" = "accession"))
+sequences <- fasta_data[seq(2, length(fasta_data), by = 2)]
 
+seq_table <- tibble(accession = sequence_names, sequence = sequences) |>
+  inner_join(genera, by = "accession")
+
+profvis::profvis(
 db <- build_kmer_database(seq_table$sequence,
                           seq_table$taxonomy,
                           kmer_size = 8)
+)
 
+microbenchmark::microbenchmark(times = 10,
+  db <- build_kmer_database(seq_table$sequence,
+                            seq_table$taxonomy,
+                            kmer_size = 8)
+)
 
 unknown_sequence <- sequences[[1]]
 bacteroides <- "TACGGAGGATTCAAGCGTTATCCGGATTTATTGGGTTTAAAGGGAGCGTAGGTGGATTGTTAAGTCAGTTGTGAAAGTTTGCGGCTCAACCGTAAAATTGCAGTTGAAACTGGCAGTCTTGAGTACAGTAGAGGTGGGCGGAATTCGTGGTGTAGCGGTTAAATGCTTAGATATCACGAAGAACTCCGATTGCGAAGGCAGCTCACTGGACTGCAACTGACACTGATGCTCGAAAGTGTGGGTATCAAACAGG"
@@ -28,10 +39,8 @@ bacteroidales <- "TACGGAGGATGCGAGCGTTATCCGGATTTATTGGGTTTAAAGGGTGCGTAGGCGGATCGTTA
 num_bootstraps <- 100
 kmer_size <- 8
 
-consensus <- classify_sequence(unknown = bacteroidales, database = db,
-                               num_bootstraps = num_bootstraps,
-                               kmer_size = kmer_size)
+classify_sequence(unknown = unknown_sequence, database = db,
+                  num_bootstraps = num_bootstraps, kmer_size = kmer_size)
 
 filtered <- filter_taxonomy(consensus)
 print_taxonomy(filtered)
-
